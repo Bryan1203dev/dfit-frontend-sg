27aca4d03817a34482b258d940a6e74719ba73b4
Bryan Padilla <ryanmusic1203@gmail.com>
2026-03-11 14:49:41 -0500

    update build

--- server.ts
+++ server.ts
@@ -495,7 +495,14 @@ async function startServer() {
       appType: 'spa',
     });
     app.use(vite.middlewares);
-  }
+  } else {
+    // Servir build de producción
+    const distPath = path.join(__dirname, 'dist');
+    app.use(express.static(distPath));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(distPath, 'index.html'));
+    });
+  } 
 
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
